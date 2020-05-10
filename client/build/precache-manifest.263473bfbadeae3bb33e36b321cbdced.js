d6a6b26892c50e36efcbf2fb504f17d91071be34
Andrii-Vodvud1 <andrii.vodvud1@ibm.com>
2020-05-10 18:24:24 +0300

    added client to git

---
+++ client/build/precache-manifest.263473bfbadeae3bb33e36b321cbdced.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fba2e1875bd958b262334998ebf2720c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1ad38483421b39880b1",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1cef2e594d64d77c6271",
+    "url": "/static/js/2.67c06e8b.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/static/js/2.67c06e8b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c1ad38483421b39880b1",
+    "url": "/static/js/main.9eb44eb2.chunk.js"
+  },
+  {
+    "revision": "5536b1ffa5673e4eb964",
+    "url": "/static/js/runtime-main.d07f956b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
